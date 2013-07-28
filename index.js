76bac6495fb4508be755f13a39e6355d061ea836
Jeff Knecht <jeff_knecht@yahoo.com>
2013-07-27 20:17:59 -0500

    Add backbone support

--- index.js
+++ index.js
@@ -13,12 +13,36 @@ app.get("/", function(req, res){
     res.render("page");
 });
 
+
+// usernames which are currently connected to the chat
+var _sockets = {};
+
+
 var io = require('socket.io').listen(app.listen(port));
 console.log("Listening on port " + port);
 
+var getUserNames = function() {
+	var userNames = [];
+	var keys = Object.keys(_sockets);
+	for (var i = 0; i < keys.length; i++) {
+		if (_sockets[keys[i]].username) {
+			userNames.push(_sockets[keys[i]].username);
+		}
+	}
+	return userNames;
+};
+
 io.sockets.on('connection', function (socket) {
-    socket.emit('message', { message: 'welcome to the chat' });
-    socket.on('send', function (data) {
-        io.sockets.emit('message', data);
-    });
+	_sockets[socket.id] = socket;
+
+	socket.on('join', function(data) {
+		socket.username = data.username;
+		io.sockets.emit('updateusers', getUserNames());		
+	});
+
+	socket.on('disconnect', function() {
+		delete _sockets[socket.id];
+	});
+
+	io.sockets.emit('updateusers', getUserNames());
 });
--- public/chat.js
+++ public/chat.js
@@ -1,25 +1,29 @@
 window.onload = function() {
  
-    var messages = [];
     var socket = io.connect('http://localhost:3700');
+    var joinButton = document.getElementById("join");
+    var identitySection = document.getElementById("identity");
+
     var field = document.getElementById("field");
     var sendButton = document.getElementById("send");
     var content = document.getElementById("content");
     var name = document.getElementById("name");
  
-    socket.on('message', function (data) {
-        if(data.message) {
-            messages.push(data);
-            var html = '';
-            for(var i=0; i<messages.length; i++) {
-                html += '<b>' + (messages[i].username ? messages[i].username : 'Server') + ': </b>';
-                html += messages[i].message + '<br />';
-            }
-            content.innerHTML = html;
-        } else {
-            console.log("There is a problem:", data);
+    socket.on('updateusers', function (data) {
+        var html = '';
+        for(var i=0; i<data.length; i++) {
+            html += '<b>' + data[i] + '<br />';
         }
+        content.innerHTML = html;
     });
+
+    joinButton.onclick = function() {
+        if(name.value == "") {
+            alert("Please type your name!");
+        } else {
+            socket.emit('join', { username: name.value });
+        }
+    };
  
     sendButton.onclick = function() {
         if(name.value == "") {
